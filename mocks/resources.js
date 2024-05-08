0ab3a3843087077ac90e2a52e0c47ab03cdb115f
ArielBetti <ariel.betti@gmail.com>
2024-05-08 18:36:50 -0300

    feature: create resources screen

---
+++ mocks/resources.js
@@ -0,0 +1,98 @@
+const resources = [
+  {
+    sector: "A1",
+    status: "ativo",
+    recurses: [
+      {
+        icon: "../../assets/energy.jpeg",
+        label: "Placas solares",
+        quantity: 332,
+        energyCust: "Baixo",
+        energyGain: "Alto",
+      },
+      {
+        icon: "../../assets/silo.jpeg",
+        label: "Silo grande",
+        quantity: 5,
+        energyCust: "Medio",
+        energyGain: "Baixo",
+      },
+      {
+        icon: "../../assets/sensor.jpeg",
+        label: "Regadores",
+        quantity: 22,
+        energyCust: "Baixo",
+        energyGain: "Baixo",
+      },
+      {
+        icon: "../../assets/sensor.jpeg",
+        label: "Sensores de umidade do ar",
+        quantity: 30,
+        energyCust: "Baixo",
+        energyGain: "Baixo",
+      },
+      {
+        icon: "../../assets/sensor.jpeg",
+        label: "Sensores de umidade da terra",
+        quantity: 100,
+        energyCust: "Baixo",
+        energyGain: "Baixo",
+      },
+      {
+        icon: "../../assets/sensor.jpeg",
+        label: "Sensores de carbono",
+        quantity: 130,
+        energyCust: "Baixo",
+        energyGain: "Baixo",
+      }
+    ],
+  },
+  {
+    sector: "A2",
+    status: "ativo",
+    recurses: [
+      {
+        icon: "../../assets/energy.jpeg",
+        label: "Placas solares",
+        quantity: 332,
+        energyCust: "Baixo",
+        energyGain: "Alto",
+      },
+      {
+        icon: "../../assets/silo.jpeg",
+        label: "Silo grande",
+        quantity: 5,
+        energyCust: "Medio",
+        energyGain: "Baixo",
+      },
+      {
+        icon: "../../assets/sensor.jpeg",
+        label: "Regadores",
+        quantity: 22,
+        energyCust: "Baixo",
+        energyGain: "Baixo",
+      },
+      {
+        icon: "../../assets/sensor.jpeg",
+        label: "Sensores de umidade do ar",
+        quantity: 30,
+        energyCust: "Baixo",
+        energyGain: "Baixo",
+      },
+      {
+        icon: "../../assets/sensor.jpeg",
+        label: "Sensores de umidade da terra",
+        quantity: 100,
+        energyCust: "Baixo",
+        energyGain: "Baixo",
+      },
+      {
+        icon: "../../assets/sensor.jpeg",
+        label: "Sensores de carbono",
+        quantity: 130,
+        energyCust: "Baixo",
+        energyGain: "Baixo",
+      }
+    ],
+  },
+]
---
+++ pages/resources/index.js
@@ -0,0 +1,65 @@
+let totalResourcesQuantity = 0;
+
+for (const sector of resources) {
+  for (const resource of sector.recurses) {
+    totalResourcesQuantity += resource.quantity;
+  }
+}
+const resourcesCounter = document.getElementById("totalResources");
+const sectorsCounter = document.getElementById("totalSectors");
+
+sectorsCounter.textContent = `Setores: ${resources.length}`;
+resourcesCounter.textContent = `Recursos: ${totalResourcesQuantity}`
+
+const tableComponent = document.getElementById("table");
+
+resources.map((item, index) => {
+  const sectorLabel = document.createElement("h2");
+  sectorLabel.classList.add("table-label")
+  const sectorStatus = document.createElement("p");
+  sectorLabel.textContent = `Setor: ${item.sector}`;
+  sectorStatus.textContent = `Status: ${item.status}`;
+  tableComponent.appendChild(sectorLabel);
+  tableComponent.appendChild(sectorStatus);
+
+  item.recurses.map((resource) => {
+    const element = document.createElement("div");
+
+    element.classList.add("table-item");
+    
+    const img = document.createElement("img");
+    
+    img.setAttribute("src", resource.icon);
+    img.classList.add("table-icon")
+    element.appendChild(img);
+    
+    const column = document.createElement("div");
+    
+    column.classList.add("column");
+    element.appendChild(column);
+    
+    const title = document.createElement("h2")
+    
+    title.classList.add("table-label");
+    title.textContent = resource.label;
+    column.appendChild(title);
+    
+    const quantityLabel = document.createElement("p");
+    
+    quantityLabel.textContent = `Quantidade: ${resource.quantity}`;
+    column.appendChild(quantityLabel);
+    
+    const energyGainLabel = document.createElement("p");
+    
+    energyGainLabel.textContent = `Ganho de energia: ${resource.energyGain}`;
+    column.appendChild(energyGainLabel);
+    
+    const energyCustLabel = document.createElement("p");
+    
+    energyCustLabel.textContent = `Gasto de energia: ${resource.energyCust}`;
+    column.appendChild(energyCustLabel);
+    tableComponent.appendChild(element);
+  });
+
+
+})
